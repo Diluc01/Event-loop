73ee288d90a96c4adec1d3cb5c3a4d6d3e93d193
Berlin Tejeda <berlintejeda22@gmail.com>
2024-10-22 18:03:26 -0400

    working on part 2

--- index.js
+++ index.js
@@ -11,3 +11,36 @@ function simpleFunction() {
 }
 
 simpleFunction();
+
+// part 2
+function flattenArr(inputArray) {
+  let outputArray = [];
+  recursion(0, inputArray, outputArray);
+  return outputArray;
+}
+
+function recursion(index, inputArray, outputArray) {
+  if (index >= inputArray.length) return;
+
+  if (Array.isArray(inputArray[index])) {
+    recursion(0, inputArray[index], outputArray);
+  } else {
+    outputArray.push(inputArray[index]);
+  }
+
+  return () => recursion(index + 1, inputArray, outputArray);
+}
+
+flatArray = flattenArr([1, 2, [3, [4, 5]], 6]);
+console.log(flatArray);
+
+function trampoline(f, ...args) {
+  let result = f(...args);
+  while (typeof result === "function") {
+    result = result();
+  }
+
+  return result;
+}
+
+console.log(trampoline(() => flattenArr([1, 2, [3, [4, 5]], 6])));
