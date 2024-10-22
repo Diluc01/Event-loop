af41dd9eb3ed5ff6d41800867b7e9a7a6f95b7ce
Berlin Tejeda <berlintejeda22@gmail.com>
2024-10-22 19:32:26 -0400

    working on part 3

--- index.js
+++ index.js
@@ -18,29 +18,51 @@ function flattenArr(inputArray) {
   recursion(0, inputArray, outputArray);
   return outputArray;
 }
-
 function recursion(index, inputArray, outputArray) {
-  if (index >= inputArray.length) return;
-
+  console.log(inputArray.length);
+  console.log(outputArray);
+  if (index >= inputArray.length) {
+    return;
+  }
   if (Array.isArray(inputArray[index])) {
+    console.log("Inside of Checking");
     recursion(0, inputArray[index], outputArray);
+    return recursion(index + 1, inputArray, outputArray);
   } else {
     outputArray.push(inputArray[index]);
+    console.log("Inside of Pushing");
+    return recursion(index + 1, inputArray, outputArray);
   }
-
-  return () => recursion(index + 1, inputArray, outputArray);
 }
-
-flatArray = flattenArr([1, 2, [3, [4, 5]], 6]);
-console.log(flatArray);
-
 function trampoline(f, ...args) {
   let result = f(...args);
   while (typeof result === "function") {
     result = result();
   }
-
   return result;
 }
-
 console.log(trampoline(() => flattenArr([1, 2, [3, [4, 5]], 6])));
+
+// part 3
+// let paragraph = document.getElementById("paragraph");
+
+function isPrime(num) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
+
+function printPrimeNumbers(n) {
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      console.log(i);
+    }
+  }
+}
+
+// printPrimeNumbers(100);
+
+// paragraph.textContent = printPrimeNumbers(100);
